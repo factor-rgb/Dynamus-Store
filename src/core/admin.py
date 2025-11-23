cd0d53c6bb653bfe9d078a897626939b00d1003d
Raul Vieira <rvieiramedrano@gmail.com>
2025-11-23 08:39:17 -0500

    feat: add initial database setup with models, migrations, and admin registration

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,134 @@
 from django.contrib import admin
+from .models import (
+    Color, Fur_shape, Pattern, Gender,
+    Employee, Eyes_color, Fur, Breed, Pet
+)
 
-# Register your models here.
+
+# <-------------------
+# Inlines
+# <-------------------
+class BreedInline(admin.TabularInline):
+    model = Breed
+    extra = 0
+    fields = ("name", "fur", "eyes_color")
+    show_change_link = True
+
+
+class PetInline(admin.TabularInline):
+    model = Pet
+    extra = 0
+    fields = ("name", "age", "gender", "entry_date", "size")
+    readonly_fields = ("entry_date",)
+    show_change_link = True
+
+
+# <-------------------
+# MODELOS SIMPLES
+# <-------------------
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Fur_shape)
+class FurShapeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Pattern)
+class PatternAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Gender)
+class GenderAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+# <-------------------
+# MODELOS COMPLEJOS
+# <-------------------
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("user", "age", "access_level")
+    search_fields = ("user__username", "user__email")
+    list_filter = ("access_level",)
+    readonly_fields = ("id",)
+    raw_id_fields = ("user",)
+    ordering = ("user__username",)
+    save_on_top = True
+
+
+@admin.register(Eyes_color)
+class EyesColorAdmin(admin.ModelAdmin):
+    list_display = ("right_eye_color", "left_eye_color", "heterochromia")
+    list_filter = ("heterochromia", "right_eye_color", "left_eye_color")
+    search_fields = (
+        "right_eye_color__name",
+        "left_eye_color__name",
+    )
+    autocomplete_fields = ("right_eye_color", "left_eye_color")
+    ordering = ("right_eye_color__name", "left_eye_color__name")
+    inlines = [BreedInline]
+    save_on_top = True
+
+
+@admin.register(Fur)
+class FurAdmin(admin.ModelAdmin):
+    list_display = ("shape", "primary_color", "secondary_color", "pattern")
+    list_filter = ("shape", "primary_color", "secondary_color", "pattern")
+    search_fields = (
+        "shape__name",
+        "primary_color__name",
+        "secondary_color__name",
+        "pattern__name",
+    )
+    autocomplete_fields = ("shape", "primary_color", "secondary_color", "pattern")
+    ordering = ("shape__name", "primary_color__name")
+    inlines = [BreedInline]
+    save_on_top = True
+
+
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    list_display = ("name", "fur", "eyes_color")
+    list_filter = ("fur", "eyes_color")
+    search_fields = (
+        "name",
+        "fur__shape__name",
+        "eyes_color__right_eye_color__name",
+    )
+    autocomplete_fields = ("fur", "eyes_color")
+    inlines = [PetInline]
+    ordering = ("name",)
+    save_on_top = True
+
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ("name", "breed", "gender", "age", "entry_date")
+    list_filter = ("breed", "gender")
+    search_fields = ("name", "breed__name")
+    autocomplete_fields = ("breed", "gender")
+    readonly_fields = ("entry_date",)
+    ordering = ("name", "-entry_date")
+    list_select_related = ("breed", "gender")
+    fieldsets = (
+        (None, {
+            "fields": ("name", "age", "breed", "gender", "size", "annotations")
+        }),
+        ("Control", {
+            "fields": ("entry_date",),
+            "description": "Fecha generada automáticamente (no editable)."
+        }),
+    )
+    save_on_top = True
--- src/core/models.py
+++ src/core/models.py
@@ -1,15 +1,156 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MaxValueValidator
 from django.db import models
+import uuid
 
-# Create your models here.
 
-# comprador, vendedor hijos de user
+class Color(models.Model):
+    name = models.CharField(max_length=30, unique=True)
 
-# comprador: username, alias, email, foring key de user
+    def __str__(self):
+        return self.name
 
-# vendedor: username, alias, email, foring key de user
 
-# categoria de productos: categoria
+class Fur_shape(models.Model):
+    name = models.CharField(max_length=30, unique=True)
 
-# producto: nombre, categoria, marca, vendedor, img, precio, descuento
+    def __str__(self):
+        return self.name
 
-# recibo: numero de recibo, vendedor, comprador, producto(s) | cantidad, fecha, precio total
+
+class Pattern(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Gender(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Gender'
+        verbose_name_plural = 'Genres'
+
+
+class Employee(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='employee')
+    age = models.PositiveSmallIntegerField(validators=[MaxValueValidator(99)])
+    access_level = models.PositiveSmallIntegerField(validators=[MaxValueValidator(5)])
+
+    def __str__(self):
+        return self.user.username
+
+
+class Eyes_color(models.Model):
+    right_eye_color = models.ForeignKey(
+        Color,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="right_eye_usages"
+    )
+
+    left_eye_color = models.ForeignKey(
+        Color,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="left_eye_usages"
+    )
+
+    heterochromia = models.BooleanField()
+
+    def __str__(self):
+        if self.right_eye_color != self.left_eye_color:
+            return f'Right {self.right_eye_color} - Left {self.left_eye_color}'
+        return f"{self.right_eye_color}"
+
+    def clean(self):
+        super().clean()
+
+        if self.left_eye_color and self.right_eye_color:
+            if self.left_eye_color != self.right_eye_color and not self.heterochromia:
+                raise ValidationError({
+                    'heterochromia': _(
+                        'Si los ojos tienen diferente color, debe activar heterocromia.'
+                    )
+                })
+
+    class Meta:
+        ordering = ('right_eye_color', 'left_eye_color')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['right_eye_color', 'left_eye_color'],
+                name='unique_eye_color_pair'
+            )
+        ]
+
+
+class Fur(models.Model):
+    shape = models.ForeignKey(Fur_shape, on_delete=models.SET_NULL, null=True)
+    primary_color = models.ForeignKey(
+        Color,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="fur_styles_usages"
+    )
+
+    secondary_color = models.ForeignKey(
+        Color,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="fur_syles_usages"
+    )
+
+    pattern = models.ForeignKey(Pattern, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.primary_color} - {self.pattern}'
+    
+    class Meta:
+        ordering = ('shape', 'primary_color', 'secondary_color', 'pattern')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['shape', 'primary_color', 'secondary_color', 'pattern'],
+                name='unique_fur_style'
+            )
+        ]
+
+
+class Breed(models.Model):
+    name = models.CharField(max_length=30)
+    fur = models.ForeignKey(Fur, on_delete=models.SET_NULL, null=True)
+    eyes_color = models.ForeignKey(Eyes_color, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.fur} - {self.eyes_color}'
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'fur', 'eyes_color'],
+                name='unique_race_characteristics'
+            )
+        ]
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=30, db_index=True)
+    age = models.PositiveSmallIntegerField(validators=[MaxValueValidator(99)])
+    breed = models.ForeignKey(Breed, on_delete=models.SET_NULL, null=True)
+    entry_date = models.DateTimeField(auto_now_add=True, editable=False)
+    gender = models.ForeignKey(Gender, on_delete=models.SET_NULL, null=True)
+    size = models.DecimalField(max_digits=4, decimal_places=2)
+    annotations = models.CharField(max_length=1000, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.breed} - {self.entry_date}'
+    
+    class Meta:
+        ordering = ('name', 'breed', '-entry_date')
