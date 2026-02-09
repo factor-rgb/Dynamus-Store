aac6455d43e90f006631ab4243ea2a642f68c05d
Raul Vieira <rvieiramedrano@gmail.com>
2026-02-09 09:06:19 -0500

    style: improve forms styles (login, register, terms_conditions, card_form, forms.py)

--- src/core/forms.py
+++ src/core/forms.py
@@ -1,59 +1,65 @@
 from typing import Any
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
-
 from django.contrib.auth.models import User
 from .models import Pet
 
 
-class CustomAuthenticationForm(AuthenticationForm):
+class StyledFormMixin:
+    def apply_styles(self):
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                "class": (
+                    "w-full border border-[#C98259] rounded-lg px-3 py-2 "
+                    "focus:outline-none focus:ring-2 focus:ring-[#C98259] "
+                    "focus:border-[#B97249] placeholder-gray-400 text-gray-800 "
+                    "transition duration-200 "
+                )
+            })
+
+
+class BaseStyledForm(forms.Form, StyledFormMixin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.apply_styles()
+
+
+class BaseStyledModelForm(forms.ModelForm, StyledFormMixin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.apply_styles()
+
+
+class CustomAuthenticationForm(BaseStyledForm, AuthenticationForm):
     class Meta:
         model = AuthenticationForm
-        fields = ['username', 'password']
-        labels = {'username': 'Nombre de Usuario'}
+        fields = ["username", "password"]
+        labels = {"username": "Nombre de Usuario"}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['username'].label = 'Nombre de Usuario'
-        self.fields['password'].label = 'Contraseña'
-
-        for field_name, field in self.fields.items():
-            field.widget.attrs.update({
-                "class": (
-                    "w-full border border-gray-300 rounded-lg px-3 py-2 "
-                    "focus:outline-none focus:ring-2 focus:ring-blue-500 "
-                    "focus:border-blue-500 placeholder-gray-400 text-gray-800"
-                ),
-                "placeholder": field.label
-            })
+        self.fields["username"].label = "Nombre de Usuario"
+        self.fields["username"].widget.attrs["placeholder"] = "ej: PetLover"
+        self.fields["password"].label = "Contraseña"
 
 
-class CustomCreationForm(UserCreationForm):
+class CustomCreationForm(BaseStyledForm, UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
-        labels = {'username': 'Nombre de Usuario'}
-        help_texts = {'username': ''}
+        fields = ["username", "password1", "password2"]
+        labels = {"username": "Nombre de Usuario"}
+        help_texts = {"username": ""}
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        self.fields['password1'].label = 'Contraseña'
-        self.fields['password2'].label = 'Confirmar Contraseña'
-        self.fields["password1"].help_text = ''
-        self.fields["password2"].help_text = ''
-
-        for field_name, field in self.fields.items():
-            field.widget.attrs.update({
-                "class": (
-                    "w-full border border-gray-300 rounded-lg px-3 py-2 "
-                    "focus:outline-none focus:ring-2 focus:ring-blue-500 "
-                    "focus:border-blue-500 placeholder-gray-400 text-gray-800"
-                ),
-                "placeholder": field.label
-            })
+        self.fields["username"].widget.attrs["placeholder"] = "ej: PetLover"
+        self.fields["password1"].label = "Contraseña"
+        self.fields["password2"].label = "Confirmar Contraseña"
+        self.fields["password1"].help_text = ""
+        self.fields["password2"].help_text = ""
 
 
-class UserProfileForm(forms.ModelForm):
+class UserProfileForm(BaseStyledModelForm):
 
     current_password = forms.CharField(
         label="Contraseña actual",
@@ -75,24 +81,16 @@ class UserProfileForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email']
-        help_texts = {'username': ''}
+        fields = ["username", "email"]
+        help_texts = {"username": ""}
     
     def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user', None)
+        self.user = kwargs.pop("user", None)
         super().__init__(*args, **kwargs)
-        self.fields['username'].label = 'Nombre de Usuario'
-        self.fields['email'].label = 'Correo Electrónico'
-
-        for field_name, field in self.fields.items():
-            field.widget.attrs.update({
-                "class": (
-                    "w-full border border-gray-300 rounded-lg px-3 py-2 "
-                    "focus:outline-none focus:ring-2 focus:ring-blue-500 "
-                    "focus:border-blue-500 placeholder-gray-400 text-gray-800"
-                ),
-                "placeholder": field.label
-            })
+        self.fields["username"].label = "Nombre de Usuario"
+        self.fields["username"].widget.attrs["placeholder"] = "ej: PetLover"
+        self.fields["email"].label = "Correo Electrónico"
+        self.fields["email"].widget.attrs["placeholder"] = "ej: petlover456@correo.com"
 
     def clean(self):
         cleaned_data = super().clean()
@@ -122,7 +120,7 @@ class UserProfileForm(forms.ModelForm):
         return user
 
 
-class PetForm(forms.ModelForm):
+class PetForm(BaseStyledModelForm):
     class Meta:
         model = Pet
         fields = [
@@ -151,24 +149,15 @@ class PetForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # Ejemplo de personalización adicional
         self.fields["name"].label = "Nombre"
         self.fields["age"].label = "Edad"
         self.fields["breed"].label = "Raza"
         self.fields["gender"].label = "Género"
         self.fields["size"].label = "Tamaño (kg)"
         self.fields["annotations"].label = "Anotaciones"
-
-        for field_name, field in self.fields.items():
-            field.widget.attrs.update({
-                "class": (
-                    "w-full border border-gray-300 rounded-lg px-3 py-2 "
-                    "focus:outline-none focus:ring-2 focus:ring-blue-500 "
-                    "focus:border-blue-500 placeholder-gray-400 text-gray-800"
-                ),
-
-                "placeholder": field.label,  # usa el label como placeholder
-            })
+        self.fields["name"].widget.attrs["placeholder"] = "Ej: Max, Copito, Zeus"
+        self.fields["size"].widget.attrs["placeholder"] = "Ej: 12.5"
+        self.fields["annotations"].widget.attrs["placeholder"] = "Ej: Alergias, comportamiento, medicación, etc."
 
     def clean_age(self):
         age = self.cleaned_data.get("age")
